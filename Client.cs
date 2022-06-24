05ef40d95742ffb19b0e5516b2aa57681789b251
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-06-24 18:41:21 +0200

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROBOT
+{
+    internal class Client
+    {
+        public HttpClient _httpClient { get; set; }
+        public Client(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+       
+    }
+}
