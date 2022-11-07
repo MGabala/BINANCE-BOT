06f6f72569cc64d9496e12fb002374d864b6e036
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-11-07 18:42:14 +0100

    update

--- Services/CRUD_TESTNET.cs
+++ Services/CRUD_TESTNET.cs
@@ -16,56 +16,71 @@
             {
                 Console.WriteLine("");
                 Console.WriteLine("Welcome in ROBOT - best cryptocurrencies bot for trading and earning money...");
-                Console.WriteLine("1: Check connection state");
+                Console.WriteLine("1: Test Connectivity");
                 Console.WriteLine("2: Check server time");
-                Console.WriteLine("3: Average price");
-                Console.WriteLine("4: Get order book");
-                Console.WriteLine("5: Get trade list");
-                Console.WriteLine("6: Get exchange information");
-                Console.WriteLine("7: Get recent trades list");
+                Console.WriteLine("3: Exchange Information");
+                Console.WriteLine("4: Order Book");
+                Console.WriteLine("5: Recent Trades List");
+                Console.WriteLine("6: Old Trade Lookup (MARKET_DATA)");
+                Console.WriteLine("7: Current Average Price");
                 Console.Write("Pick number to get method: ");
                 int input = Convert.ToInt32(Console.ReadLine());
 
                 switch (input)
                 {
                     case 1:
-                        await GETCheckConnectionState();
+                        await GETTestConnectivity();
                         break;
                     case 2:
                         await GETCheckServerTime();
                         break;
                     case 3:
-                        await GETCoinPrice();
+                        await GETExchangeInformation();
                         break;
                     case 4:
                         await GETOrderBook();
                         break;
                     case 5:
-                        await GETTradeList();
+                        await GETRecentTradeList();
                         break;
                     case 6:
-                        await GETExchangeInformation();
+                        await GETOldTradeLookup();
                         break;
                     case 7:
-                        await GETRecentTradesList();
+                        await GETCurrentAveragePrice();
                         break;
+
                     default:
+                        Console.WriteLine($"Sorry, {input} not supported yet. Please choose another number.");
                         break;
                 }
             }
         }
-        private async Task GETRecentTradesList()
+        private async Task GETTestConnectivity()
         {
-            Console.Write("Choose pair: ");
-            var pair = Console.ReadLine();
             try
             {
-                var response = await _httpClient.GetAsync($"/api/v3/trades?symbol={pair}");
+                var response = await _httpClient.GetAsync("/api/v3/ping");
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine($"Connection status: {response.StatusCode}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Sorry, cannot proceed your request..");
+                Console.WriteLine(exception.Message);
+            }
+
+        }
+        private async Task GETCheckServerTime()
+        {
+            try
+            {
+                var response = await _httpClient.GetAsync("/api/v3/time");
                 response.EnsureSuccessStatusCode();
                 var content = await response.Content.ReadAsStringAsync();
                 Console.WriteLine($"{content.ToString()}");
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
                 Console.WriteLine("Sorry, cannot proceed your request..");
                 Console.WriteLine(exception.Message);
@@ -83,64 +98,11 @@
                 var content = await response.Content.ReadAsStringAsync();
                 Console.WriteLine($"{content.ToString()}");
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
                 Console.WriteLine("Sorry, cannot proceed your request..");
                 Console.WriteLine(exception.Message);
             }
-
-        }
-        private async Task GETCheckServerTime()
-        {
-            try
-            {
-                var response = await _httpClient.GetAsync("/api/v3/time");
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"{content.ToString()}");
-            }
-            catch(Exception exception)
-            {
-                Console.WriteLine("Sorry, cannot proceed your request..");
-                Console.WriteLine(exception.Message);
-            }
-
-        }
-
-        private async Task GETCheckConnectionState()
-        {
-            try
-            {
-                var response = await _httpClient.GetAsync("/api/v3/ping");
-                response.EnsureSuccessStatusCode();
-                Console.WriteLine($"Connection status: {response.StatusCode}");
-            }
-            catch(Exception exception)
-            {
-                Console.WriteLine("Sorry, cannot proceed your request..");
-                Console.WriteLine(exception.Message);
-            }
-
-        }
-
-        private async Task GETCoinPrice()
-        {
-            Console.Write("Choose pair: ");
-            var pair = Console.ReadLine();
-
-            try
-            {
-                var response = await _httpClient.GetAsync($"/api/v3/avgPrice?symbol={pair}");
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Average price for {pair}:  {content.ToString()}");
-            }
-            catch(Exception exception)
-            {
-                Console.WriteLine("Sorry, cannot proceed your request..");
-                Console.WriteLine(exception.Message);
-            }
-
         }
         private async Task GETOrderBook()
         {
@@ -154,14 +116,14 @@
                 Console.WriteLine($"Order Book Depth: {content.Count()}");
                 Console.WriteLine(content.ToString());
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
                 Console.WriteLine("Sorry, cannot proceed your request..");
                 Console.WriteLine(exception.Message);
             }
 
         }
-        private async Task GETTradeList()
+        private async Task GETRecentTradeList()
         {
             Console.Write("Choose pair: ");
             var pair = Console.ReadLine();
@@ -172,11 +134,49 @@
                 var content = await response.Content.ReadAsStringAsync();
                 Console.WriteLine($"Trade list: {content.ToString()}");
             }
-            catch(Exception exception)
+            catch (Exception exception)
             {
                 Console.WriteLine("Sorry, cannot proceed your request..");
                 Console.WriteLine(exception.Message);
             }
         }
+        private async Task GETOldTradeLookup()
+        {
+            Console.Write("Choose pair: ");
+            var pair = Console.ReadLine();
+            try
+            {
+                var response = await _httpClient.GetAsync($"/api/v3/historicalTrades?symbol={pair}");
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"{content.ToString()}");
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine("Sorry, cannot proceed your request..");
+                Console.WriteLine(exception.Message);
+            }
+
+        }
+        private async Task GETCurrentAveragePrice()
+        {
+            Console.Write("Choose pair: ");
+            var pair = Console.ReadLine();
+
+            try
+            {
+                var response = await _httpClient.GetAsync($"/api/v3/avgPrice?symbol={pair}");
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Average price for {pair}:  {content.ToString()}");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Sorry, cannot proceed your request..");
+                Console.WriteLine(exception.Message);
+            }
+
+        }
+
     }
 }
